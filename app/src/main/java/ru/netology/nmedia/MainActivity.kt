d1a3b47fb4e3255e821e680108a66437973a832a
ERokhmanko <rizhii_mishonok@mail.ru>
2021-08-10 15:44:47 +0300

    Merge constraint layout

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,53 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущег",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb\"",
+            published = "21 мая в 18:36",
+        )
+
+//        findViewById<ImageButton>(R.id.like).setOnClickListener {
+//            (it as? ImageButton)?.setImageResource(R.drawable.ic_baseline_favorite_24)
+//        }
+        //        использование кликлистнера без синтетика/КАЕ
+
+
+//        like.setOnClickListener {
+//            like.setImageResource(R.drawable.ic_baseline_favorite_24)
+//        }
+        // работа с синтетиком. Минусы: Если лайк будет null, то приложение упадет. Поэтому сделаем через VIEW BINDING ниже
+
+        with(binding){
+            author.text = post.author
+            content.text = post.content
+            published.text = post.published
+
+            if (post.likedByMe){
+                like?.setImageResource(R.drawable.ic_baseline_favorite_24)
+            }
+
+           like?.setOnClickListener {
+               post.likedByMe = !post.likedByMe
+                like.setImageResource(if (post.likedByMe) R.drawable.ic_baseline_favorite_24
+                else R.drawable.ic_baseline_favorite_border_24)
+            }
+        }
+
     }
 }
+
+
